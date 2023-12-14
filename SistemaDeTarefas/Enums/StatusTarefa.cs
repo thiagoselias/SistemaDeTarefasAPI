c61efec8435c1f21ab3830e3a598e37145225687
Thiago Elias <thiago.s.elias@hotmail.com>
2023-12-14 14:28:09 -0400

    API Sistema de Tarefas

---
+++ SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        AFazer = 1,
+        [Description("Em Andamento")]
+        EmAndamento = 2,
+        [Description("concluído")]
+        Concluido = 3,
+    }
+}
